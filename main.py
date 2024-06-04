51d83b180e9cd3c4d2a20dfe41bae874c2bfb015
Ishman <ishman.bahrha@gmail.com>
2024-06-04 23:50:45 +0530

    Creating Project Base

---
+++ main.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+st.title('Tarot')
+st.subheader('_Welcome to your digital Tarot Deck_', divider='blue')
+
+# Buttons
+blankcol, buttoncol = st.columns([1,1])
+
+with blankcol:
+    st.write("")
+with buttoncol:
+    st.button(":blue[Shuffle Deck]")
+
+
+# Cards
+images = ["assets/Fool.jpeg",
+          "assets/HighPriestess.jpeg",
+          "assets/World.jpeg"]
+
+col1, col2, col3 = st.columns([2,2,1])
+
+with col1:
+    st.image(images[1], width = 250)
+
+with col2:
+    st.image(images[0], width = 250)
+
+with col3:
+    st.image(images[2], width = 250)
